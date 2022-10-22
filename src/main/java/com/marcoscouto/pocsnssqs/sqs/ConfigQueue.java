4cd02558aba2a0bdc091114544befce4f6dbaac1
Marcos Couto <marcosmartinellicouto@gmail.com>
2022-10-22 17:24:17 -0300

    chore: config sqs client and create queue

---
+++ src/main/java/com/marcoscouto/pocsnssqs/sqs/ConfigQueue.java
@@ -0,0 +1,33 @@
+package com.marcoscouto.pocsnssqs.sqs;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.services.sqs.SqsClient;
+import software.amazon.awssdk.services.sqs.model.CreateQueueRequest;
+
+import javax.annotation.PostConstruct;
+
+@Slf4j
+@Configuration
+public class ConfigQueue {
+
+    private final SqsClient client;
+    @Value("${aws.sqs.queue.name}")
+    private String queueName;
+
+    public ConfigQueue(SqsClient client) {
+        this.client = client;
+    }
+
+    @PostConstruct
+    public void createQueue() {
+        log.info("[SQS] creating queue: {}", this.queueName);
+        var request = CreateQueueRequest.builder().queueName("poc-sqs").build();
+
+        var response = this.client.createQueue(request);
+
+        log.info("[SQS] Queue created successfully. Url: {}", response.queueUrl());
+    }
+
+}
---
+++ src/main/java/com/marcoscouto/pocsnssqs/sqs/ConfigSQSClient.java
@@ -0,0 +1,22 @@
+package com.marcoscouto.pocsnssqs.sqs;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.services.sqs.SqsClient;
+
+import static software.amazon.awssdk.regions.Region.SA_EAST_1;
+
+@Slf4j
+@Configuration
+public class ConfigSQSClient {
+
+    @Bean
+    public SqsClient configClient() {
+        log.info("[SQS] Configuring SqsClient bean");
+        return SqsClient.builder()
+            .region(SA_EAST_1)
+            .build();
+    }
+
+}
