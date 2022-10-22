561a5b4ff5c76a1def6d1377507a9c16b8c681cf
Marcos Couto <marcosmartinellicouto@gmail.com>
2022-10-22 10:02:09 -0300

    first commit

---
+++ src/main/java/com/marcoscouto/pocsnssqs/PocSnsSqsApplication.java
@@ -0,0 +1,13 @@
+package com.marcoscouto.pocsnssqs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocSnsSqsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocSnsSqsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/marcoscouto/pocsnssqs/PocSnsSqsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcoscouto.pocsnssqs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocSnsSqsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
